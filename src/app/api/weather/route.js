c64753048f839af65af7332da4eee5c566c86b3e
byQuexo <66476537+byQuexo@users.noreply.github.com>
2023-10-24 22:15:21 +0200

    added query handeling

--- src/app/api/weather/route.js
+++ src/app/api/weather/route.js
@@ -1,3 +1,6 @@
-export async function GET(request) {
-    return new Response("HALLO");
+export async function GET(req, res) {
+    const url = new URL(req.url);
+    const city = url.searchParams.get("city");
+    console.log(city);
+    return Response.json({ city });
 }
