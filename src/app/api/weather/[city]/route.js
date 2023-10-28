3c2021d4f2ca114aab30b911944d134625b08c3a
byQuexo <66476537+byQuexo@users.noreply.github.com>
2023-10-28 13:29:37 +0200

    Upgraded Next.js

--- src/app/api/weather/[city]/route.js
+++ src/app/api/weather/[city]/route.js
@@ -1,18 +1,16 @@
-import fs from 'fs/promises';
-import path from 'path';
-const __dirname = path.resolve();
+//import fs from 'fs/promises';
+//import path from 'path';
+//const __dirname = path.resolve();
 
-export async function GET(req, res) {
-    return new Response(JSON.stringify({ city: city }), {
-        headers: {
-            'content-type': 'application/json'
-        }
-    });
+export async function GET(request) {
+    request.GET = request.query;
+    return new Response(`Hello ${city}!`);
 }
 
 
 // const { city } = req.query;
 //     const sendData = async () => {
+
 //         try {
 //             const filePath = path.join(__dirname, `./weatherData/${city.toLowerCase()}.json`);
 //             console.log(filePath);
