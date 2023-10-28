2eafb31e99c29eaf73967a131972ecc25f1efbe8
byQuexo <66476537+byQuexo@users.noreply.github.com>
2023-10-28 14:51:26 +0200

    added getWeather functions

--- src/app/Functions/index.js
+++ src/app/Functions/index.js
@@ -1,1 +1,9 @@
- 
+export async function getWeather(city) {
+    const res = await fetch(`http://localhost:3000/api/weather/${city}`).catch((err) => {
+        return err;
+    }).then((res) => {
+        return res.json();
+    });
+
+    return await res;
+}
