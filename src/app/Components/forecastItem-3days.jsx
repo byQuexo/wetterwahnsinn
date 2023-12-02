f4f312bacbd6e25a127c6e84e06f260bf1db0a44
byQuexo <66476537+byQuexo@users.noreply.github.com>
2023-12-02 20:35:31 +0100

    Added media queue

--- src/app/Components/forecastItem-3days.jsx
+++ src/app/Components/forecastItem-3days.jsx
@@ -4,12 +4,12 @@ export default function ForecastItem({day, icon, tempmin, tempmax, description})
     return (
         <>
         <div className="flex flex-row gap-4 items-center w-fit">
-            <span className="text-left text-white">{day}</span>
-            <img className="w-14 h-14" src={icon} alt={day} />
-            <span className="text-white w-fit">{description}</span>
-            <span className="text-left text-white">{tempmin}</span>
+            <span className="text-left text-white text-bold md:text-xl">{day}</span>
+            <img className="w-14 h-14 md:w-20 md:h-20" src={icon} alt={day} />
+            <span className="text-white w-fit text-bold md:text-xl">{description}</span>
+            <span className="text-left text-white md:text-lg">{tempmin}</span>
             <br />
-            <span className="text-left text-white">{tempmax}</span>
+            <span className="text-left text-white md:text-lg">{tempmax}</span>
         </div>
         </>
     )
--- src/app/page.js
+++ src/app/page.js
@@ -108,7 +108,7 @@ export default function Home() {
           <ComboboxWithAutocomplete onSearch={addToSearchHistory} onLocation={setztLocation}/>
         </div>
         <div className="flex-1 gap-2 w-fit ">
-          <div className="grid grid-cols-1 md:grid-cols-2  gap-4 h-4/5 w-fit" style={{ position: "absolute", overflowY: "scroll" }}> 
+          <div className="grid grid-cols-1 md:grid-cols-2 gap-4 h-4/5 w-fit" style={{ position: "absolute", overflowY: "scroll" }}> 
               <SearchHistory history={searchHistory} remove={removeFromSearchHistory}/>
               <Weather location={location}/>
           </div>
