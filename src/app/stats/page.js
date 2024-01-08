ce17df78b8f637547f2c585b24f669fab739123f
ncdavid2 <David.Werner@edu.bib.de>
2024-01-08 11:14:54 +0100

    Background rework

--- src/app/stats/page.js
+++ src/app/stats/page.js
@@ -54,14 +54,14 @@ export default function StatsPage() {
             />
           </div>
           <div className="flex-1 gap-2">
-          <div className="h-4/5 w-5/6" style={{ backgroundColor: "#808080", margin: "20px", padding: "20px" }}>
+          <div className="h-5/6 w-5/6" style={{ backgroundColor: "#808080", margin: "20px", padding: "10px", borderRadius: "12px" }}>
               <h1 style={{ color: "#000000" }}>Stats der letzten 50 Jahre: {countryn}</h1>
               <LineChart
                 width={1150}
                 height={450}
                 data={data}
                 margin={{ top: 20, right: 50, bottom: 50, left: 0 }}
-                style={{ backgroundColor: "#f0f0f0", padding: "8px", borderRadius: "12px" }}>
+                style={{ backgroundColor: "#f0f0f0", padding: "20px", margin: "15px", borderRadius: "12px" }}>
                 <Line type="monotone" dataKey="Wärme" stroke="#8884d8" />
                 <CartesianGrid stroke="#ccc" strokeDasharray="5 5" />
                 <XAxis dataKey="Jahr" />
